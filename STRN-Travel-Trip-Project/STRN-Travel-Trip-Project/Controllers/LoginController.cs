3e81afac3c5ce78b700a5001975902830a440c91
Ruveyda Hilal <rhakcinar@gmail.com>
2022-03-31 19:56:13 +0300

    final

---
+++ STRN-Travel-Trip-Project/STRN-Travel-Trip-Project/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using STRN_Travel_Trip_Project.Models.Siniflar;
+
+namespace STRN_Travel_Trip_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin ad)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
