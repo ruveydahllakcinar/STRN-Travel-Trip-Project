5a91053a6550fc480a4f1362b7834ca0e9ede9d2
Ruveyda Hilal <rhakcinar@gmail.com>
2022-04-01 01:11:55 +0300

    contact finish

---
+++ STRN-Travel-Trip-Project/STRN-Travel-Trip-Project/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STRN_Travel_Trip_Project.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
